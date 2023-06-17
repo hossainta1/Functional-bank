eb1f8965627549bc57962e89a5401a4055aa4b3a
hossainta1 <tanvir.hossain303030@gmail.com>
2023-06-18 00:42:00 +0600

    Added deposite calculation

--- js/deposite.js
+++ js/deposite.js
@@ -1,1 +1,37 @@
-console.log('deposite file added');
+/*
+1. Add event listener to deposite button
+2. get deposite amount from the deposite input field
+2.5 Converting deposite string in to number format
+3.clear the deposite input field by geating value.
+
+4. get the previous deposite total
+
+5. Calculate new deposite total
+
+
+
+*/
+
+document.getElementById('btn-deposite').addEventListener('click', function () {
+    // step-2
+    const depositeField = document.getElementById('deposite-field');
+    const newDepositeAmountString = depositeField.value;
+    const newDepositeAmount = parseFloat(newDepositeAmountString);
+    // step 3 : 
+    depositeField.value = '';
+
+    // step-4
+
+    const depositeTotalElement = document.getElementById('deposite-total');
+    const previousDepositeTotalString = depositeTotalElement.innerText;
+    const previousDepositeTota = parseFloat(previousDepositeTotalString);
+
+
+    // Step 5 
+
+    const newDepositeTotal = previousDepositeTota + newDepositeAmount;
+
+    depositeTotalElement.innerText = newDepositeTotal;
+
+
+})
