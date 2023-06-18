174e39733ccabaf5e7e0f10a29210b73a96acb5d
hossainta1 <tanvir.hossain303030@gmail.com>
2023-06-18 11:46:42 +0600

    Withdraw calculation added

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,26 @@
-console.log('withdraw file added');
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    withdrawField.value = '';
+
+
+    const withdrawTotalElement = document.getElementById('Withdraw-total')
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotalElement.innerText = newWithdrawTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+})
